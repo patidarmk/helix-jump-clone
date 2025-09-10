c8afcedfa1babfc823873af0f9b2f71c725813fb
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-10 16:45:58 +0530

    Add src/components/Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Header } from './Header';
+import { MadeWithApplaa } from './made-with-applaa';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="relative flex min-h-screen w-full flex-col bg-background">
+       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
+      <Header />
+      <main className="flex-1">{children}</main>
+      <MadeWithApplaa />
+    </div>
+  );
+};
